922a6c18c281ce4ef2350bf878eb9b8ceca91adf
GitAyan <endecipher@gmail.com>
2018-03-18 23:47:24 +0530

    Added PHP SQL Prelim code

--- index.php
+++ index.php
@@ -163,5 +163,35 @@ if(isset($_SESSION['username'])) {
 	</div> <!-- end content -->
 <!-- There are some issue-->
 
+<?php
+/*
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "gsms";
+
+
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+
+$conn = OpenCon();
+echo "Connected Successfully"; 
+CloseCon($conn);  
+
+//Add the SQL Query code
+
+*/
+?>
+
 </body>
 </html>
