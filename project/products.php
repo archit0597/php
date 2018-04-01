e1d0f0b57c7cb0c2321c0d91633082c1f13b2763
GitAyan <endecipher@gmail.com>
2018-04-02 00:39:49 +0530

    Products cart session and good border

--- project/products.php
+++ project/products.php
@@ -3,7 +3,24 @@
    $_session["order_id"]="101";
    $_session["cust_id"]="100";
    $d=date("Y-m-d");
-   $_session["order_date"]=$d; 
+   $_session["order_date"]=$d;
+   if(!isset($_SESSION["cart"]))
+   $_SESSION["cart"]=array();
+   
+   if(isset($_GET["cart"])){
+	  if(in_array($_GET["cart"],$_SESSION["cart"])!=""){ //already there in session array
+		  //skip it
+		  echo "Already in cart";
+		  echo "<div id='myModal' class='modal'><div class='modal-content'><span class='close'><a href='/products.php'>&times;</a></span><p>Already Added in Cart.</p></div></div>";
+	  }else{
+		  array_push($_SESSION["cart"],$_GET["cart"]);
+		  echo "Added to cart";
+	  }
+	  if($_GET["cart"]=="destroy")
+		  session_destroy();
+	  
+	  print_r($_SESSION["cart"]); 
+   }
 ?>
 <!DOCTYPE html>
 <?php
@@ -43,10 +60,50 @@ width: 100%;
 height: auto;	
 }
 .somecont{
-
 margin-left:5%;
 margin-right:5%;
-	
+}
+.atc{
+margin-bottom:1em;
+}
+.already > div > .there{	
+border: 0.5em solid rgba(120,220,120,0.99);
+}
+.there{
+border: 0.5em solid rgba(220,220,220,0.99);	
+}
+.modal {
+    display: block; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+.modal-content {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+}
+.close {
+    color: #aaaaaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
 }
 </style>
 	</head>
@@ -80,8 +137,14 @@ if($result){
 			echo '<div class="card-deck">';	
 		}
 		//echo '<div class="col-sm-3 col-xs-12"><div class="card"><img class="card-img-top" src="'.$row["pimgurl"].$row["pmodel"].$row["pbrand"].$row["pprice"].$row["ptype"];
-		echo '<div class="col-sm-3 col-xs-12"><div class="card maker"><img class="image" src="pics/'.$row["pimgurl"].'.jpg" alt="'.$row["pbrand"].' '.$row["pmodel"].'"><div class="card-block exsp"><h4 class="card-title">'.$row["pbrand"].' '.$row["pmodel"].'</h4><p class="card-text">'.$row["ptype"]." ".$row["pprice"].'</p><br><input style="margin-top:-4em;" id="'.$row["pid"].'" type="button" onclick="cartFunc(this)" class="btn btn-success btn-sm" value="Add to Cart"></div></div></div>';
-        if($q%4==0){
+		if(in_array($row["pid"],$_SESSION["cart"])){
+			echo "<div class='already'>";
+		}
+		echo '<div class="col-sm-3 col-xs-12"><div class="card maker there"><img class="image" src="pics/'.$row["pimgurl"].'.jpg" alt="'.$row["pbrand"].' '.$row["pmodel"].'"><div class="card-block exsp"><h4 class="card-title">'.$row["pbrand"].' '.$row["pmodel"].'</h4><p class="card-text">'.$row["ptype"]." ".$row["pprice"].'</p><input id="'.$row["pid"].'" type="button" onclick="cartFunc(this)" class="btn btn-success btn-sm atc" value="Add to Cart"></div></div></div>';
+        if(in_array($row["pid"],$_SESSION["cart"])){
+			echo "</div>";
+		}
+		if($q%4==0){
 				echo "</div></div>";
 				$val=$val-1;
 		}
@@ -104,7 +167,7 @@ if ($result->num_rows > 0) {
 </div>
 <script>
 	function cartFunc(object){
-		alert(object.id);
+		document.location.href="products.php?cart="+object.id;
 	}
 	
 	
@@ -124,6 +187,7 @@ if ($result->num_rows > 0) {
 	$('#password').blur(function() {
 		$('label[for="password"]').removeClass('selected');
 	});
+	
 </script>
 
 </body>
