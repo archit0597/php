a2173dea2e73c663f44db3fc143bf3e0be8d1dd9
GitAyan <endecipher@gmail.com>
2018-04-05 21:44:46 +0530

    Modified Profile with working Backend Details.

--- project/profile.php
+++ project/profile.php
@@ -20,7 +20,7 @@
 
 <div class="container">
   <div class="jumbotron jumbotron-fluid">
-    <?php
+<?php
     $servername='localhost';
     $username='root';
     $password='';
@@ -31,10 +31,19 @@
       echo 'Could not select database';
       exit;
     }
-//$query1 =mysqli_query($link,"SELECT * FROM cust WHERE custemail='".$username."'");
-//$jf1 = mysqli_fetch_assoc($query1);
+	$cid=$_SESSION["details"][0];
+	$sql="select * from cust where custid='".$cid."'";
+	$result=mysqli_query($link,$sql);
+	if($result){
+	  while(($row = $result->fetch_assoc())) {
+		  echo "<h2> CustID=".$row["custid"]." and Name=".$row["custname"]." and EMail=".$row["custemail"]." and Phone=".$row["custphone"]." and Adress=".$row["custaddress"]." and State=".$row["custstate"]."</h2>";
+	  }
+	}else{
+	  echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+	}
+	
 ?>
-	<ul style="list-style-type:none">
+	<!--ul style="list-style-type:none">
        
 	   <li>Name</li>
 	   <li>E-Mail</li>
@@ -51,25 +60,39 @@
 							<th style="width:22%; color:black" class="text-center">Total</th>
 							<th style="width:10%"color:black" class="text-center">Order Date</th>
 						</tr>
-					</thead>
+					</thead-->
 					
 	<?php
-	foreach ($_SESSION["cart"] as $x){
-	$sql="select * from orders inner join order_items on orders.orderid = order_items.orderid inner join products on order_items.pid = products.pid";
 	
+	$sql="select o.orderid 'orderid' , o.ostatus 'ostatus', o.odate 'odate', oi.pid 'pid', products.pmodel 'pmodel', products.pprice 'pprice' ,products.pbrand 'pbrand', products.pimgurl 'pimgurl', oi.quantity 'q', oi.total 'total' from orders o ,order_items oi,products where oi.orderid=o.orderid and o.custid='".$cid."' and products.pid=oi.pid";
 	$result=mysqli_query($link,$sql);
+	$dummy=0;
 	if($result){
+			if($result->num_rows==0){
+				echo "<b>You have no orders.</b><hr><br>";
+			}else
 	  while(($row = $result->fetch_assoc())) {
-					echo '<tbody><tr id="temprow"><td data-th="Product"><div class="row"><div class="col-sm-2 hidden-xs">';
-					//echo '<img src="pics/'.$row["pimgurl"].'.jpg" class="img-responsive"/></div>';
-					echo '<div class="col-sm-10"><h4 class="nomargin">'.$row["pbrand"]." ".$row["pmodel"].'</h4></div></div>';
-					echo '</td><td data-th="Price"><span  style="color=#000000" id="c'.$row["pid"].'">'.$row["quantity"].'</font></td><td data-th="Quantity">'; /*.$_SESSION["cartq"][$count].*/
-					echo '</td><td data-th="Subtotal" class="text-center"><span style="color=#000000;" class="subtotal" id="b'.$row["pid"].'">'.$row["total"].'</span></td>';
-					//echo '<button id="'.$row["pid"].'" class="btn btn-danger btn-sm" onclick="myfunc(this)"><i class="fa fa-trash-o"></i></button></td></tr></tbody>';
+					if($dummy==0){
+						$dummy=$row["orderid"];
+							
+							echo '<h4>Here are all the details of the order of OrderID='.$row["orderid"]." and Order Status=".$row["ostatus"].' and Order Date='.$row["odate"].'</h4>';
+					}else{
+						if($row["orderid"]==$dummy){
+							echo '<hr>'; echo '<br>';
+						}
+					}
+					
+					echo '<img src="pics/'.$row["pimgurl"].'.jpg" class="img-responsive" style="height: 100px; width: 100px;"/>'; echo '<br>';
+					echo '<h4>Product ID='.$row["pid"].'</h4>';echo '<br>';
+					echo '<h4>Name='.$row["pbrand"]." ".$row["pmodel"].'</h4>';echo '<br>';
+					echo '<h4>Unit Price='.$row["pprice"].'</h4>'; echo '<br>';
+					echo '<h4>Quantity='.$row["q"].'</h4>'; echo '<br>';
+					echo '<h4>Subtotal='.$row["total"].'</h4>'; echo '<br>';
+					
+					
 	  }
 	}else{
 	  echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-	}
 	}
 	?>
 	</table>
